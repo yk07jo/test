3905af06c52101b2da3ffdfe67f2f7180e6d0efb
yk07jo <ojts0314.us@gmail.com>
2018-11-30 00:24:38 +0900

    add ex.py

--- money.py
+++ money.py
@@ -1,17 +1,23 @@
 import random
+import sys
 
-capital = 100000
-win_per = 0.55
+capital = 1000000
+win_per = 0.5
 bet = 1000
 
-for i in range(365):
+for i in range(10000):
     rnd = random.randint(1, 100)
-    if capital < bet:
+    if capital < 1000:
         break
+    bet = 1000
     if rnd < 100*win_per:
         flag = "Win"
         capital += bet*0.85
     else:
         flag = "Lose"
         capital -= bet
-    print("i={:3d}:{:4s} {}".format(i+1, flag, int(capital)))
+    print("i={:6d} : {:4s} {:6d}".format(i+1, flag, int(capital)))
+    #if (i+1) % 100 == 0:
+        #print("i={:6d}:{}".format(i+1, int(capital)))
+        #sys.stdout.write("\ri={}:{}".format(i+1, int(capital)))
+        #sys.stdout.flush()
---
+++ paiza/ex.py
@@ -0,0 +1,33 @@
+def search(array, w, x):  #この関数で下にあるブロックの高さを求める
+  a = [0] #幅それぞれの深さ(全部0の時のためとりあえず0いれとく)
+  for i in range(x, x+w): #ブロック左から左
+    for j in range(len(array)): #フィールドの高さ
+      if array[j][i] != 0:   #もしブロックがすでにあればその高さをaに追加
+        a.append(j)
+        break
+  if len(a) != 1: #リストの大きさが1でなければさっきの0削除
+    del a[0]
+  return min(a) #aの最小値 = すでにあるブロックの最も高いやつの高さを返す
+
+def fill(array, h, w, x, b):  #この関数でブロックの存在を確立
+  for i in range(b-h, b):
+    for j in range(x, x+w):
+      array[i][j] = 1 #ブロックがあるところは0へ
+
+H, W, N = map(int, input().split()) #H, W, Nの入力
+h, w, x = [[0 for i in range(N)] for j in range(3)] #h, w, xの空リスト生成
+for i in range(N): #h, w, xの入力
+  h[i], w[i], x[i] = map(int, input().split())
+
+S = [[0 for i in range(W)] for j in range(H)] #フィールド生成
+for i in range(N):
+  fill(S, h[i], w[i], x[i], search(S, w[i], x[i]))  #埋める関数
+
+
+for i in range(H):
+  for j in range(W):
+    if S[i][j] == 0: #0ならば.を
+      print(".", end = "")
+    else: #そうでなければ#を
+      print("#", end = "")
+  print()
